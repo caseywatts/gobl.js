1da87f4339d045dc6679e2a95019dd24de3d46ab
Casey Watts <casey.s.watts@gmail.com>
2022-05-10 15:00:22 -0400

    comment out initial testing code

--- wasm/index.js
+++ wasm/index.js
@@ -4,18 +4,18 @@ window.gobl = {};
 window.gobl.keygen = keygen;
 window.gobl.build = build;
 
-const result = await keygen();
-console.log(`RESULT: ${result}`);
+// const result = await keygen();
+// console.log(`RESULT: ${result}`);
 
-try {
-    const buildResult = await build({
-        "data": {},
-        "privateykey": {},
-    });
-    console.log(`BUILD RESULT: ${build_result}`);
-} catch (e) {
-    console.log("BUILD ERROR: " + e)
-};
+// try {
+//     const buildResult = await build({
+//         "data": {},
+//         "privateykey": {},
+//     });
+//     console.log(`BUILD RESULT: ${build_result}`);
+// } catch (e) {
+//     console.log("BUILD ERROR: " + e)
+// };
 
-const result2 = await keygen();
-console.log(`RESULT2: ${result2}`);
+// const result2 = await keygen();
+// console.log(`RESULT2: ${result2}`);
