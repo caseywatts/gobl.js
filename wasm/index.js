ca7535e03f95647bd8ba46ccfd694e2733e05065
Casey Watts <casey.s.watts@gmail.com>
2022-05-12 11:54:59 -0400

    install prettier and eslint; fix files to match these

--- wasm/index.js
+++ wasm/index.js
@@ -25,59 +25,61 @@ exampleInputs.noSchema = `{
 const exampleData = exampleInputs.simpleMessage;
 
 const generateAndDisplayKey = async () => {
-    const key = await keygen();
-    goblData.key = JSON.parse(key);
-    document.getElementById("key").value = key;
-}
+  const key = await keygen();
+  goblData.key = JSON.parse(key);
+  document.getElementById("key").value = key;
+};
 
-const displayExampleInputFile = async () => {
-    document.getElementById("input-file").value = exampleData;
-}
+// this is hard coded on the index page now
+// const displayExampleInputFile = async () => {
+//     document.getElementById("input-file").value = exampleData;
+// }
 
 const processInputFile = async () => {
-    const inputFile = document.getElementById("input-file").value;
+  const inputFile = document.getElementById("input-file").value;
 
-    const buildData = {
-        data: JSON.parse(inputFile),
-        privatekey: goblData.key.private
-    }
+  const buildData = {
+    data: JSON.parse(inputFile),
+    privatekey: goblData.key.private,
+  };
 
-    try { 
-        const buildResult = await build(buildData);
-        document.getElementById("output-file").value = buildResult;
-        updateStatus("success");
-    } catch (e) {
-        document.getElementById("output-file").value = "";
-        updateStatus("error", e);
-    }
-}
-
+  try {
+    const buildResult = await build(buildData);
+    document.getElementById("output-file").value = buildResult;
+    updateStatus("success");
+  } catch (e) {
+    document.getElementById("output-file").value = "";
+    updateStatus("error", e);
+  }
+};
 
 const markSuccess = (el) => {
-    el.classList.remove("bg-red-200")
-    el.classList.add("bg-green-200")
-}
+  el.classList.remove("bg-red-200");
+  el.classList.add("bg-green-200");
+};
 
 const markError = (el) => {
-    el.classList.add("bg-red-200")
-    el.classList.remove("bg-green-200")
-}
+  el.classList.add("bg-red-200");
+  el.classList.remove("bg-green-200");
+};
 
 const updateStatus = async (type, message) => {
-    const statusEl = document.getElementById("status");
-    if (type === "success") {
-        statusEl.innerHTML = "Success!"
-        markSuccess(statusEl)
-    } else { // error case
-        statusEl.innerHTML = `Error: ${message}`
-        markError(statusEl)
-    }
-}
+  const statusEl = document.getElementById("status");
+  if (type === "success") {
+    statusEl.innerHTML = "Success!";
+    markSuccess(statusEl);
+  } else {
+    // error case
+    statusEl.innerHTML = `Error: ${message}`;
+    markError(statusEl);
+  }
+};
 
 await generateAndDisplayKey();
 // await displayExampleInputFile();
 await processInputFile();
 
-document.getElementById("input-file").oninput = function updateOnInputFileChange () {
+document.getElementById("input-file").oninput =
+  function updateOnInputFileChange() {
     processInputFile();
-};
+  };
