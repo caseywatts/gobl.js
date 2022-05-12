c68a1945572439966b77493d0dde908667cac88e
Casey Watts <casey.s.watts@gmail.com>
2022-05-12 09:43:10 -0400

    comment cleanup

--- wasm/index.js
+++ wasm/index.js
@@ -1,24 +1,9 @@
 import { keygen, build } from "./gobl.js";
 
+// assigning these to the global namespace for cypress tests
 window.gobl = {};
 window.gobl.keygen = keygen;
 window.gobl.build = build;
-
-// const result = await keygen();
-// console.log(`RESULT: ${result}`);
-
-// try {
-//     const buildResult = await build({
-//         "data": {},
-//         "privateykey": {},
-//     });
-//     console.log(`BUILD RESULT: ${build_result}`);
-// } catch (e) {
-//     console.log("BUILD ERROR: " + e)
-// };
-
-// const result2 = await keygen();
-// console.log(`RESULT2: ${result2}`);
 
 let goblData = {};
 
